b892e76ff72028ed5b57aa6676bf5858b47bb5ae
Juan Pablo Olivadoti Locatelli <juan.olivadoti@codere.com>
2023-10-04 09:32:52 +0200

    Repo test 1

---
+++ andes-new-style.js
@@ -0,0 +1,48 @@
+window.onload = () => {
+    var headTag = document.getElementsByTagName("head")[0];
+    var linkforCSSfile = document.createElement("link");
+    linkforCSSfile.href = "https://www.andesempanadas.com/wp-content/uploads/2023/10/andes-new-style.css";
+    linkforCSSfile.type = "text/css";
+    linkforCSSfile.rel = "stylesheet";
+    headTag.appendChild(linkforCSSfile);
+    document.body.appendChild(headTag);
+};
+
+(function (w, s) {
+    var linkforCSSfile = document.createElement("link");
+    linkforCSSfile.href = "https://www.andesempanadas.com/wp-content/uploads/2023/10/andes-new-style.css";
+    linkforCSSfile.type = "text/css";
+    linkforCSSfile.rel = "stylesheet";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(e, x);
+})(window, "script");
+
+(function (w, s) {
+
+   var googleapis = document.createElement("link");
+   googleapis.rel = "preconnect";
+    googleapis.href = "https://fonts.googleapis.com";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(googleapis, x);
+
+    var gstatic = document.createElement("link");
+    gstatic.rel = "preconnect";
+    gstatic.crossOrigin = "anonymous";
+    gstatic.href = "https://fonts.gstatic.com";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(gstatic, x);
+
+     var googleSourceCode = document.createElement("link");
+     googleSourceCode.rel = "stylesheet";
+     googleSourceCode.href = "https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,400;0,800;1,400;1,800&display=swap";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(googleSourceCode, x);
+
+    var linkforCSSfile = document.createElement("link");
+    linkforCSSfile.rel = "stylesheet";
+    linkforCSSfile.type = "text/css";
+    linkforCSSfile.href = "https://www.andesempanadas.com/wp-content/uploads/2023/10/andes-new-style.css";
+    var x = document.getElementsByTagName("script")[0];
+    x.parentNode.insertBefore(linkforCSSfile, x);
+
+})(window, "script");
